0b2c03407d62c0208135b810410b9dd59565d19b
ag1910 <58875503+ag1910@users.noreply.github.com>
2020-06-07 16:46:51 +0300

    index.php güncellendi

--- index.php
+++ index.php
@@ -17,6 +17,22 @@ elseif(isset($_POST['bol']))
 {
 	echo $_POST['birincisayi'] / $_POST['ikincisayi'];
 }
+elseif(isset($_POST['alan']))
+{
+	echo $_POST['birincisayi'] * $_POST['ikincisayi'];
+}
+elseif(isset($_POST['alan1']))
+{
+	echo $_POST['birincisayi'] + $_POST['birincisayi'] * $_POST['ikincisayi'] / 100;
+}
+elseif(isset($_POST['alan2']))
+{
+	echo $_POST['birincisayi'] - $_POST['birincisayi'] * $_POST['ikincisayi'] / 100;
+}
+elseif(isset($_POST['alan3']))
+{
+	echo $_POST['birincisayi'] * $_POST['ikincisayi'] / 100;
+}
 
 else {
 
@@ -24,14 +40,18 @@ else {
 
 <form name="form1" method="post" action="">
   <p>
-    <label for="birincisayi">Birinci Sayı:</label>
+    <label for="birincisayi">X:</label>
     <input type="text" name="birincisayi">
   </p>
   <p>
-    <label for="ikincisayi">İkinci Sayı:</label>
+    <label for="ikincisayi">Y:</label>
     <input type="text" name="ikincisayi">
 		<br>
 				<br>
+					<input type="submit" name="alan3" value="x'in %y'si kactır?">
+						<input type="submit" name="alan2" value="x, %y kadar azaltılırsa kaç olur?">
+							<input type="submit" name="alan1" value="x, %y kadar artırılırsa kaç olur?">
+		<input type="submit" name="alan" value="Alan">
     <input type="submit" name="topla" value="Topla">
     <input type="submit" name="cikar"  value="Çıkar">
     <input type="submit" name="carp" value="Çarp">
